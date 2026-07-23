780802d7216862658ef481d8c567b1634db0d6c0
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-07-23 15:40:51 +0000

    text-elements.py: simple demo created

---
+++ text-elements.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import os
+import sys
+
+if not st.runtime.exists():
+    cmd = ["bash", "run.sh", *sys.argv]
+    os.execvp(cmd[0], cmd)
+
+st.title("Title")
+st.header("Header")
+st.subheader("Subheader")
+st.markdown("**Markdown**")
+st.caption("caption")
+st.divider()
+
+with open("text-elements.py") as fp:
+    st.code(fp.read(), "python")
