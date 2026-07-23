5c68e6b275d092cb14f9919e97e8dd49b9a79b78
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-07-23 15:31:51 +0000

    hello-world.py: self-contained execution mechanism added

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import os
+import sys
 
-st.title("hello world")
-st.write("Welcome to the hello world app")
+if not st.runtime.exists():
+    cmd = ["bash", "run.sh", *sys.argv]
+    os.execvp(cmd[0], cmd)
+
+st.title("Hello world")
+for k, v in sorted(os.environ.items()):
+    st.markdown(f"- {k}: {v}")
