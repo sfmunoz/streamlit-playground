75f007909ec5d3af3c26a93c463f6b59b74626cc
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-07-25 07:36:34 +0000

    multi-page: '1_page1.py' divided in two subpages

--- multi-page/pages/1_page1.py
+++ multi-page/pages/1_page1.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
-st.set_page_config("the 1st page")
-st.title("page-1")
-st.write("this is the 1st page")
+
+def page_a():
+    st.set_page_config("page-1a")
+    st.title("page-1a")
+    st.write("page-1a")
+
+
+def page_b():
+    st.set_page_config("page-1b")
+    st.title("page-1b")
+    st.write("page-1b")
+
+
+page_names_to_funcs = {
+    "page-a": page_a,
+    "page-b": page_b,
+}
+
+demo_name = st.sidebar.selectbox("select a subpage", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
