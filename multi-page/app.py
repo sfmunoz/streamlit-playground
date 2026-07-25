74908e2a7533cd3f3aa26cf8d097791976f232d5
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-07-25 07:20:23 +0000

    multi-page: simple demo created

---
+++ multi-page/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import os
+import sys
+
+if not st.runtime.exists():
+    cmd = ["bash", "run.sh", *sys.argv]
+    os.execvp(cmd[0], cmd)
+
+st.title("Multipage")
+st.write("ref: https://docs.streamlit.io/get-started/tutorials/create-a-multipage-app")
---
+++ multi-page/pages/2_page2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("page-2")
+st.write("this is the 2nd page")
